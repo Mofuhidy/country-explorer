d8883f2147f98b8a4a2e1638dcb257a0cd144da5
moFuhidy <mo7ammed9290@gmail.com>
2023-07-20 12:40:42 +0300

    Remove uneccssary data

--- src/components/CountryBox.jsx
+++ src/components/CountryBox.jsx
@@ -1,25 +1,27 @@
 import { useNavigate } from "react-router-dom";
-import yemen from "../assets/tn_ym-flag.png";
+
 import { FiArrowRightCircle } from "@react-icons/all-files/fi/FiArrowRightCircle";
-function CountryBox() {
+function CountryBox({ flag, name, capital, pop, id, alt }) {
   const navigate = useNavigate();
 
   return (
     <>
-      <div className="country-box col-span-1 text-white p-4 relative flex flex-col">
+      <div
+        className="country-box col-span-1 text-white p-4 relative flex flex-col"
+        id={id}>
         <button type="button" onClick={() => navigate(`/conutrydata/`)}>
           <FiArrowRightCircle className=" absolute right-2 top-1 text-xl" />
         </button>
         <div className=" h-20 w-20 self-center">
-          <img src={yemen} alt="yemen" />
+          <img src={flag} alt={alt} />
         </div>
         <div className="smallDetails self-end">
           <p className="countryName font-bold uppercase text-right text-xl">
-            Yemen
+            {name}
           </p>
-          <p className="countryName font-light text-sm text-right">Sanaa</p>
+          <p className="countryName font-light text-sm text-right">{capital}</p>
           <p className="population  text-sm">
-            population: <span className=" font-bold text-base">30M</span>
+            population: <span className=" font-bold text-base">{pop}</span>
           </p>
         </div>
       </div>
